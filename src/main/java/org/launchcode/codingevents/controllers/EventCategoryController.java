8f378f05f79f3f448daf22ff7b2ae7f96aceaf5a
JBuck31 <jrbuck31@gmail.com>
2020-01-21 20:09:44 -0600

    created render and process CreateEventCategoryForm

--- src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventCategoryController.java
@@ -5,11 +5,15 @@ import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @RequestMapping("eventCategories")
-public class EventCategoryController {
+public class EventCategoryController<newEventCategory> {
 
     @Autowired
     private EventCategoryRepository eventCategoryRepository;
@@ -19,5 +23,25 @@ public class EventCategoryController {
         model.addAttribute("title", "All Categories");
         model.addAttribute("categories", eventCategoryRepository.findAll());
         return "eventCategories/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEventCategoryForm(Model model) {
+        model.addAttribute("title", "Create Category");
+        model.addAttribute(new EventCategory());
+        return "eventCategories/create";
+    }
+
+    @PostMapping
+    public String processCreateEventCategoryForm(Model model, @Valid EventCategory newEventCategory, Errors errors){
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Category");
+            model.addAttribute("errorMsg", "Bad data!");
+            return "eventCategories/create";
+        }
+
+        eventCategoryRepository.save(newEventCategory);
+        return "redirect";
+
     }
 }
